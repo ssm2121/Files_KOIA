ff0b4df62d89378ca32acef920dc74a5ab23758d
ssm2121 <102584993+ssm2121@users.noreply.github.com>
2022-07-12 10:54:58 +0300

    Add files via upload

--- Files_KOIA/TagAll.lua
+++ Files_KOIA/TagAll.lua
@@ -7,35 +7,26 @@ if Devmuh:get(KOIA.."muh:TagAll:Time"..msg.chat_id_..':'..msg.sender_user_id_) t
 Dev_muh(msg.chat_id_, msg.id_, 1, '⌔︙انتظر دقيقه بعد ارسال الامر', 1, 'md')
 return false  
 end
-if Redis:get(KOIA.."lockalllll") == "off" then
-return LuaTele.sendText(msg.chat_id,msg.id,'*⌔︙تم تعطيل @all من قبل المطور الاساسي\n*',"md",true)  
+Devmuh:setex(KOIA..'muh:TagAll:Time'..msg.chat_id_..':'..msg.sender_user_id_,300,true)
+tdcli_function({ID="GetChannelFull",channel_id_ = msg.chat_id_:gsub('-100','')},function(arg,data) 
+tdcli_function({ID = "GetChannelMembers",channel_id_ = msg.chat_id_:gsub('-100',''), offset_ = 0,limit_ = data.member_count_},function(arg,dp)
+x = 0
+tags = 0
+local list = dp.members_
+for k, v in pairs(list) do
+tdcli_function({ID="GetUser",user_id_ = v.user_id_},function(extra,result,success)
+if x == 5 or x == tags or k == 0 then
+tags = x + 5
+t = "#all"
 end
-local Info_Members = LuaTele.searchChatMembers(msg.chat_id, "*", 200)
-x = 0 
-tags = 0 
-local list = Info_Members.members
-for k, v in pairs(list) do 
-local data = LuaTele.getUser(v.member_id.user_id)
-if x == 5 or x == tags or k == 0 then 
-tags = x + 5 
-if ttag then
-t = "#all "..ttag.."" 
-else
-t = "#all "
-end
-end 
-x = x + 1 
-TagName = data.first_name
-TagName = tagname:gsub("]","") 
-TagName = tagname:gsub("[[]","") 
-t = t..", ["..tagname.."](tg://user?id="..v.member_id.user_id..")" 
-if x == 5 or x == tags or k == 0 then 
-if ttag then
-Text = t:gsub('#all '..ttag..',','#all '..ttag..'\n') 
-else 
-Text = t:gsub('#all,','#all\n')
-end
-LuaTele.sendText(msg.chat_id,0,Text,'md',true)
+x = x + 1
+TagName = result.first_name_
+TagName = TagName:gsub("]","")
+TagName = TagName:gsub("[[]","")
+t = t..", ["..TagName.."](tg://user?id="..v.user_id_..")"
+if x == 1 or x == tags or k == 0 then
+local Text = t:gsub('#all,','#all\n')
+SendText(msg.chat_id_,Text,msg.id_/2097152/0.5,'md')
 end
 end,nil)
 end
