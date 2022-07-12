bbcf8356ce3ab9a9cd09d23b49c19b02404cfda4
ssm2121 <102584993+ssm2121@users.noreply.github.com>
2022-07-12 10:52:45 +0300

    Add files via upload

--- Files_KOIA/TagAll.lua
+++ Files_KOIA/TagAll.lua
@@ -7,12 +7,9 @@ if Devmuh:get(KOIA.."muh:TagAll:Time"..msg.chat_id_..':'..msg.sender_user_id_) t
 Dev_muh(msg.chat_id_, msg.id_, 1, '⌔︙انتظر دقيقه بعد ارسال الامر', 1, 'md')
 return false  
 end
-if text:match("^all (.*)$") or text:match("^@all (.*)$") or text == "@all" or text == "all" then 
-local ttag = text:match("^all (.*)$") or text:match("^@all (.*)$") 
-if not msg.Manager then
-return LuaTele.sendText(msg.chat_id,msg.id,Reply_Status(msg.sender_id.user_id,'⌔︙هذا الامر يخص ↫ '..Controller_Num(7)..' .\n').Warning,"md",true)    
-end
 if Redis:get(KOIA.."lockalllll") == "off" then
+return LuaTele.sendText(msg.chat_id,msg.id,'*⌔︙تم تعطيل @all من قبل المطور الاساسي\n*',"md",true)  
+end
 local Info_Members = LuaTele.searchChatMembers(msg.chat_id, "*", 200)
 x = 0 
 tags = 0 
@@ -28,9 +25,9 @@ t = "#all "
 end
 end 
 x = x + 1 
-tagname = data.first_name
-tagname = tagname:gsub("]","") 
-tagname = tagname:gsub("[[]","") 
+TagName = data.first_name
+TagName = tagname:gsub("]","") 
+TagName = tagname:gsub("[[]","") 
 t = t..", ["..tagname.."](tg://user?id="..v.member_id.user_id..")" 
 if x == 5 or x == tags or k == 0 then 
 if ttag then
@@ -39,9 +36,13 @@ else
 Text = t:gsub('#all,','#all\n')
 end
 LuaTele.sendText(msg.chat_id,0,Text,'md',true)
-end 
-end 
-end 
+end
+end,nil)
+end
+end,nil)
+end,nil)
+end
+end
 end
 
 end
